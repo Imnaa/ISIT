9100b285a7115a5fa3a0fc8f5fe7e0c8620caa55
Ромащенко Н.А <kolya.romashenko@yandex.ru>
2019-12-05 12:51:13 +0300

    123

---
+++ ISIT/Discipline.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISIT
+{
+    class Discipline
+    {
+        public int number { get; set; }
+        private int credits { get; set; }
+        private int exams { get; set; }
+        private int zachets { get; set; }
+        private int[] influence { get; set; }
+        private string name { get; set; }
+
+        public Discipline(int _number)
+        {
+            this.number = _number;
+
+            // здесь чтение из файла
+        }
+
+
+
+
+    }
+}
--- ISIT/Program.cs
+++ ISIT/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ISIT
 {
@@ -7,10 +9,60 @@ namespace ISIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int count = 22;
+            /*объявление всех предметов*/
+            Discipline[] shedule = new Discipline[count];
+            for (int i = 0; i < count; i++)
+            {
+                /* считвыаем из файла и кидаем в ()*/
+                shedule[i] = new Discipline(i+1);
+                /*заполняем влияние предмета на предметы*/
+            }
 
+            foreach (var arr in AllPermutations(shedule))
+            {
+                foreach (var i in arr)
+                    Console.Write(i + " ");
+                Console.WriteLine();
+            }
 
+            /*перестановка всех предметов
+                проверка того, что предмет удволетворяет критериям
+                    если удволетворяет, то выводим в файл
+             */
 
+            /*Критерии
+             *  Ограничение по кредитам - 30 шт.
+             *  Название предметов в семестре не должны повторяться
+             *  Коэф влияния итоговый > 0.75*количество предметов
+             */
             Console.Read();
+        }
+        static bool Next(ref Discipline[] arr)
+        {
+            int k, j, l;
+            for (j = arr.Length - 2; (j >= 0) && (arr[j].number >= arr[j + 1].number); j--) { }
+            if (j == -1)
+            {
+                arr = arr.OrderBy(c => c).ToArray();
+                return false;
+            }
+            for (l = arr.Length - 1; (arr[j].number >= arr[l].number) && (l >= 0); l--) { }
+            var tmp = arr[j];
+            arr[j] = arr[l];
+            arr[l] = tmp;
+            for (k = j + 1, l = arr.Length - 1; k < l; k++, l--)
+            {
+                tmp = arr[k];
+                arr[k] = arr[l];
+                arr[l] = tmp;
+            }
+            return true;
+        }
+        static IEnumerable<Discipline[]> AllPermutations(Discipline[] arr)
+        {
+            do yield return arr;
+            while (Next(ref arr));
         }
     }
 }
